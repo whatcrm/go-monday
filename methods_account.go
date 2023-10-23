681499d258c8d613310815160931087f67b1c55d
lanyere <agzam.erlan@gmail.com>
2023-10-23 14:22:53 +0600

    add Create/Delete Webhook

--- methods_account.go
+++ methods_account.go
@@ -33,3 +33,51 @@ func (c *Get) Account() (out models.Account, err error) {
 	out = query.Account
 	return
 }
+
+type Webhook struct {
+	BoardID int    `graphql:"board_id"`
+	Config  string `graphql:"config"`
+	Event   string `graphql:"event"`
+	ID      int    `graphql:"id"`
+}
+type WebhookEventType string
+
+func (m *Mutate) CreateWebhook(boardID, uri string, event WebhookEventType) (err error) {
+	var mutation struct {
+		Webhook Webhook `graphql:"create_webhook (board_id: $board_id url: $url event: $event)"`
+	}
+
+	variables := map[string]interface{}{
+		"board_id": boardID,
+		"url":      uri,
+		"event":    event,
+	}
+
+	options := makeRequestOptions{
+		BaseURL:   mondayAPI,
+		Mutation:  mutation,
+		Variables: variables,
+	}
+
+	err = m.api.makeRequest(options)
+	return
+}
+
+func (m *Mutate) DeleteWebhook(id ID) (err error) {
+	var mutation struct {
+		Webhook Webhook `graphql:"delete_webhook (id: $id)"`
+	}
+
+	variables := map[string]interface{}{
+		"id": id,
+	}
+
+	options := makeRequestOptions{
+		BaseURL:   mondayAPI,
+		Mutation:  mutation,
+		Variables: variables,
+	}
+
+	err = m.api.makeRequest(options)
+	return
+}
