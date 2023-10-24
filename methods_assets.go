7a019ee6ca931d12d416e2272609990d24aa0dc3
lanyere <agzam.erlan@gmail.com>
2023-10-24 10:52:50 +0600

    add get Assets

---
+++ methods_assets.go
@@ -0,0 +1,23 @@
+package monday
+
+import "github.com/whatcrm/go-monday/models"
+
+func (c *Get) Assets(id ID) (out models.Assets, err error) {
+	var query struct {
+		Assets models.Assets `graphql:"assets (ids: $id)"`
+	}
+
+	variables := map[string]interface{}{
+		"id": id,
+	}
+
+	options := makeRequestOptions{
+		BaseURL:   mondayAPI,
+		Query:     &query,
+		Variables: variables,
+	}
+
+	err = c.api.makeRequest(options)
+	out = query.Assets
+	return
+}
